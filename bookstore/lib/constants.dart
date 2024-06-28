4d22d355aaec4c322ed1f1479aeb29001d540d87
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-06-27 23:45:06 -0300

    [ :gear: ] | setup: const - colors (app::design)

--- bookstore/lib/constants.dart
+++ bookstore/lib/constants.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
 
+const BLACK = Color(0xFF393939);
+const LIGHT_BLACK = Color(0xFF8F8F8F);
+const ICON_COLOR = Color(0xFFF48A37);
+const PROGRESS_INDICATOR = Color(0xFFBE7066);
+
+final SHADOW_COLOR = Color(0xFFD3D3D3).withOpacity(.84);
